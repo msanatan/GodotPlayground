f2c3f451be824e0dc53874e2eb047ad565f310e4
Marcus Sanatan <msanatan@gmail.com>
2021-12-15 23:47:49 -0400

    Set up a basic UI to load the mini games I'll create

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_QuitButton_pressed():
+	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
